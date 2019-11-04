62362bdad8fb943bbc431992c95ed509af7af6e8
minwoox <songmw725@gmail.com>
2019-11-04 14:46:31 +0900

    Update Armeria version

--- src/test/java/FutureTest.java
+++ src/test/java/FutureTest.java
@@ -1,21 +1,14 @@
-import java.util.concurrent.CompletableFuture;
-
 import org.junit.jupiter.api.Test;
 
 class FutureTest {
 
     @Test
     void futureCallback() {
-        System.err.println("Thread: " + Thread.currentThread().getName());
 
-        final CompletableFuture<Void> future = new CompletableFuture<>();
-        final Thread thread = new Thread(() -> future.complete(null));
-        thread.setName("t1");
-        thread.start();
+    }
 
-        future.handle((unused, cause) -> {
-            System.err.println("In handle. Thread: " + Thread.currentThread().getName());
-            return null;
-        });
+    @Test
+    void nettyFutureCallback() {
+
     }
 }
